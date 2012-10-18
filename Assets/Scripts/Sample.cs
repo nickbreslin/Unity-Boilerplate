a190df9d64eff96992f28ddc00ecb43f1b86a6db
Nick Breslin <nickbreslin@gmail.com>
2012-10-17 21:11:07 -0700

    Purpose: Switching comment style to XML. Fixes #1

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -1,42 +1,55 @@
-//==============================================================================
-// Project: Class
-//------------------------------------------------------------------------------
-// Copyright 2012 Copyrightholder
-// Licensed under The MIT License
+//
+// Sample.js
+//
+// Author: Nick Breslin (nickbreslin@gmail.com)
+// Copyright (c) 2012
+// Licensed under the MIT License
 // http://opensource.org/licenses/MIT
-//==============================================================================
+//
 
 using UnityEngine;
 using System.Collections;
 
-//==============================================================================
-// Class
-//------------------------------------------------------------------------------
-// Description
-//==============================================================================
+/// <summary>
+/// Class description
+/// </summary>
 
 public class Sample : MonoBehaviour
 {
-    //--------------------------------------------------------------------------
-    // Static Properties
-    //--------------------------------------------------------------------------
-
-    //--------------------------------------------------------------------------
-    // Public Properties
-    //--------------------------------------------------------------------------
+	/// <summary>
+	/// The instance.
+	/// </summary>
+	public static Sample instance;
 	
-	//--------------------------------------------------------------------------
-    // Private Properties
-    //--------------------------------------------------------------------------
-
-    //==========================================================================
-    // Method Name
-    //--------------------------------------------------------------------------
-    // Description
-    //--------------------------------------------------------------------------
-    //      parameters:     int input
-    //
-    //      return:         int output
-    //
-    //==========================================================================
+	/// <summary>
+	/// The counter.
+	/// </summary>
+	public int counter;
+	
+	/// <summary>
+	/// The _value.
+	/// </summary>
+	private int _counter;
+	
+	/// <summary>
+	/// Gets the counter.
+	/// </summary>
+	/// <returns>
+	/// The counter.
+	/// </returns>
+	public int getCounter()
+	{
+		return this.counter;
+	}
+	
+	/// <summary>
+	/// Sets the counter.
+	/// </summary>
+	/// <param name='value'>
+	/// Value.
+	/// </param>
+	public void setCounter(int value)
+	{
+		this.counter = value;
+	}
 }
